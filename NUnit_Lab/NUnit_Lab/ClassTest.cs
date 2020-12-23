66878b8fce1b5df3f902fe6b628073ae71f524ec
Magiske <nastya.borzenko10@mail.ru>
2020-12-23 20:54:42 +0700

    Лаба сделана

---
+++ NUnit_Lab/NUnit_Lab/ClassTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// подключение NUnit
+using NUnit.Framework;
+
+namespace NUnit_Lab
+{
+    // атрибут, указывающий на то, что это класс содержит тесты
+    [TestFixture]
+    class ClassTest
+    {
+        [TestCase]
+        public void InchToCm()
+        {
+            Functions f = new Functions();
+            Assert.AreEqual(7.62, f.InchToCm(3));
+
+            var ex = Assert.Throws<ArgumentException>(() => f.InchToCm(-3));
+            Assert.That(ex.Message, Is.EqualTo("Входящее значение не может быть меньше, либо равным нулю"));
+        }
+
+        [TestCase]
+        public void IsEvenNumber()
+        {
+            Functions f = new Functions();
+            Assert.AreEqual(true, f.IsEvenNumber(4));
+            Assert.AreEqual(false, f.IsEvenNumber(9));
+            Assert.AreEqual(true, f.IsEvenNumber(-8));
+            Assert.AreEqual(false, f.IsEvenNumber(-7));
+        }
+
+        [TestCase]
+        public void MaxElementOnArray()
+        {
+            int[] arr = new int[] { };
+            int[] arr1 = new int[] { 1, 2, 3, 4, 5 };
+            int[] arr2 = new int[] { -1, -2, -3, -4, -5 };
+
+            Functions f = new Functions();
+            Assert.AreEqual(5, f.MaxElementOnArray(arr1));
+            Assert.AreEqual(-1, f.MaxElementOnArray(arr2));
+            var ex = Assert.Throws<Exception>(() => f.MaxElementOnArray(arr));
+            Assert.That(ex.Message, Is.EqualTo("Пустой массив"));
+        }
+
+        [TestCase]
+        public void RemainerOfDivision()
+        {
+            Functions f = new Functions();
+            Assert.AreEqual(0, f.RemainerOfDivision(-2, 2));
+            Assert.AreEqual(1, f.RemainerOfDivision(5, 2));
+            Assert.AreEqual(2, f.RemainerOfDivision(5, 3));
+            // получение исключения
+            var exception = Assert.Throws <Exception> (() => f.RemainerOfDivision(2, 0));
+            // сравнение полученного сообщения с ожидаемым
+            Assert.That(exception.Message, Is.EqualTo("Делитель не должен быть равен нулю"));
+            // проверка выполняется успешно, если исключение не было сгенерировано
+            Assert.DoesNotThrow(() => f.RemainerOfDivision(2, 1));
+
+        }
+    }
+}
---
+++ NUnit_Lab/NUnit_Lab/Functions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit_Lab
+{
+    class Functions
+    {
+        public double InchToCm(double inch)
+        {
+            if (inch <= 0)
+            {
+                throw new ArgumentException("Входящее значение не может быть меньше, либо равным нулю");
+            }
+            return (inch * 2.54);
+        }
+
+        public bool IsEvenNumber(int num1)
+        {
+            if (num1 % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public int MaxElementOnArray(int[] arr)
+        {
+            if (arr.Length == 0)
+                throw new Exception("Пустой массив");
+
+            int max = arr[0];
+
+            for (int i = 0; i <= arr.Length - 1; i++)
+            {
+                if (max < arr[i])
+                    max = arr[i];
+                else
+                    continue;
+            }
+            return max;
+        }
+
+        public int RemainerOfDivision(int num1, int num2)
+        {
+            if (num2 == 0)
+                throw new Exception("Делитель не должен быть равен нулю");
+            return num1 % num2;
+        }
+    }
+}
